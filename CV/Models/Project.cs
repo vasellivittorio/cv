b0bc50dbd053eb5c899125d06136d955d33f325a
Vittorio <vittorio.vaselli@outlook.it>
2017-02-06 21:53:36 +0000

    Create project model

---
+++ CV/Models/Project.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CV.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+        public int ExperienceId { get; set; }
+    }
+}
