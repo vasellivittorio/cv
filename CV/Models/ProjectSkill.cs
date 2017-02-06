b64fdbe233d6a3256f5ee125413d3c40e512891d
Vittorio <vittorio.vaselli@outlook.it>
2017-02-06 22:12:43 +0000

    Create project-skill model

---
+++ CV/Models/ProjectSkill.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CV.Models
+{
+    public class ProjectSkill
+    {
+        public int SkillId { get; set; }
+        public int ProjectId { get; set; }
+        public string HowSkillIsUsed { get; set; }
+    }
+}
