0f0331214c5d48fcbd07fdb2c324cc21a4ee44cb
Vittorio <vittorio.vaselli@outlook.it>
2017-02-06 22:32:07 +0000

    Add education model

---
+++ CV/Models/Education.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CV.Models
+{
+    public class Education
+    {
+        public string Istitution { get; set; }
+        public string Title { get; set; }
+    }
+}
