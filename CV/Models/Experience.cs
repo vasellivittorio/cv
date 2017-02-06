ab4e46cb9bf4bcc913e0c412904ce23125b7fb35
Vittorio <vittorio.vaselli@outlook.it>
2017-02-06 21:46:35 +0000

    Create experience model

---
+++ CV/Models/Experience.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CV.Models
+{
+    public class Experience
+    {
+        public string Role { get; set; }
+        public string Company { get; set; }
+        public string City { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
