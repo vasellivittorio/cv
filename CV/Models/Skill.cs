b404e807b28d57ada7ba622f6273e664b4885043
Vittorio <vittorio.vaselli@outlook.it>
2017-02-06 21:49:22 +0000

    Create skill model

---
+++ CV/Models/Skill.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CV.Models
+{
+    public class Skill
+    {
+        public int id { get; set; }
+        public int Name { get; set; }
+    }
+}
